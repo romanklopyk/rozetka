9a08f16432ee62384cca3dbe4cef2925e270655a
romanklopyk <86305348+romanklopyk@users.noreply.github.com>
2021-07-01 20:10:45 +0300

    категорії товарів

--- js/app.js
+++ js/app.js
@@ -1,17 +1,22 @@
-
-$(document).on('click', '.header__search-form-but', function(){
-	var x =$('.header__search-input input').val();
-	alert (x);
+$(document).on('click', '.header__search-form-but', function () {
+    var x = $('.header__search-input input').val();
+    alert(x);
 });
 
-$(document).on('click', '.header__menu-toggle', function(){
-	$(this).toggleClass('opened');
-	$('.slide-menu').toggleClass('opened');
+$(document).on('click', '.header__menu-toggle', function () {
+    $(this).toggleClass('opened');
+    $('.slide-menu').toggleClass('opened');
 
 });
 
 
+$(document).ready(function () {
+    var sliderOptions = {
+        autoplay: true,
+        prevArrow: '<div class="prev-button"><svg><use href="#chevron-right"></use></svg></div>',
+        nextArrow: '<div class="next-button"><svg><use href="#chevron-right"></use></svg></div>'
 
-
-
-
+    };
+    
+    $('.home__banner-slider').slick(sliderOptions)
+});
--- js/myslick.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $('.home__banner-slider').slick({
-      autoplay: true,
-      arrows: false
-      
-  });
-});
